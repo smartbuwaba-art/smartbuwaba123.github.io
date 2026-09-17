1260e3d6c3359a243881fd530ec97595c6fb4e03
smartbuwaba-art <smartbuwaba@gmail.com>
2026-09-17 13:31:28 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'smart-buwaba-app-v38-navigation-fix';
+const CACHE = 'smart-buwaba-app-v39-stable';
 const APP_SHELL = ['./','./index.html','./manifest.webmanifest','./logo.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
