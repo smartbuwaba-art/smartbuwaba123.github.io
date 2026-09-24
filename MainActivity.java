0d4a152d935eead98869665c30f7fa3e0a17e9e8
smartbuwaba-art <smartbuwaba@gmail.com>
2026-09-24 09:42:07 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends Activity {
             }
         });
         requestNeededPermissions();
-        webView.loadUrl("https://smartbuwaba-art.github.io/smartbuwaba123.github.io/");
+        webView.loadUrl("file:///android_asset/index.html");
     }
     private void requestNeededPermissions() {
         if (android.os.Build.VERSION.SDK_INT >= 23) {
